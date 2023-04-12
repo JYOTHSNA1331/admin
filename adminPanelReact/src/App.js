5f64565b5ac1bb284f761b5f06c98fb81e2c3a2e
JYOTHSNA1331 <60293594+JYOTHSNA1331@users.noreply.github.com>
2023-04-12 15:23:11 +0530

    Add files via upload

---
+++ adminPanelReact/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Admin, Resource, EditGuesser } from "react-admin";
+import { UserList, UserEdit, UserCreate } from './users';
+import authProvider from './authProvider'
+import jsonServerProvider from "ra-data-json-server";
+
+const dataProvider =
+  jsonServerProvider("https://jsonplaceholder.typicode.com");
+
+class App extends Component {
+  render() {
+    return (
+      <Admin dataProvider={dataProvider} authProvider={authProvider}>
+        <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate}/>
+      </Admin>
+    );
+  }
+}
+export default App;
